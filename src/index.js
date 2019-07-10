cd368e6b8b97476a94c01c93381c0eb21b28cf30
reade <reade@helpshift.com>
2019-07-10 14:01:33 -0700

    SMS Chat for issues

--- src/index.js
+++ src/index.js
@@ -17,39 +17,144 @@ app.use(bodyParser.json());
 import twilio from 'twilio';
 const twilioClient = twilio(env['TWILIO_ACCOUNT_SID'], env['TWILIO_AUTH_TOKEN']);
 
+const CHECK_INTERVAL = 2500;
+let intervals = {};
+
 // webhook to recieve message
-app.post('/incoming-sms', async (req, res) => {
+app.post('/incoming-sms', async ({ body }, res) => {
   try {
-    let { body } = req;
-    // get user info here
-    // either start new issue or 
+    let issue;
 
-    let response = await fetch(`https://api.helpshift.com/v1/${env['HELPSHIFT_DOMAIN']}/issues`, {
-      headers: {
-        Accept: "application/json",
-        Authorization: `Basic ${Buffer.from(env['HELPSHIFT_API_KEY']).toString('base64')}`
-      },
-      method: "POST",
-      body: getFormData({
-        'platform-id': env['HELPSHIFT_PLATFORM_ID'],
-        'identifier': body['From'],
-        'message-body': body['Body']
-      })
-    });
-    let json = await response.json();
+    let { issues } = await getExistingIssue(body['From'])
+    if (issues && issues.length > 0) {
+      // send to existing issue
+      await sendMessageToAgent(issues[0]['id'], body)
+    } else {
+      issue = await createIssue(body);
+      // listen to messages from issue
+      setupMessageListener(issue);
+    }
 
-
-    res.send(json);
+    res.send();
   } catch (error) {
     console.log('ERROR', error);
     res.status(400).send(error); 
   }
 });
 
+async function getExistingIssue(userId) {
+  let params = new URLSearchParams({ 
+    identifier: userId,
+    "platform-id": env['HELPSHIFT_PLATFORM_ID'],
+    state: ['new', 'new-for-agent', 'agent-replied', 'waiting-for-agent', 'pending-reassignment']
+  });
+  let response = await fetch(`https://api.helpshift.com/v1/${env['HELPSHIFT_DOMAIN']}/chat/my-issues?${params.toString()}`, {
+    headers: {
+      Accept: "application/json",
+      Authorization: `Basic ${Buffer.from(env['HELPSHIFT_API_KEY']).toString('base64')}`
+    }
+  });
+  return await response.json();
+}
+
+async function createIssue({ From, Body }) {
+  
+  /********
+  *********
+
+  get user info here to add userId and CIFs
+
+  *********
+  ********/
+
+  let response = await fetch(`https://api.helpshift.com/v1/${env['HELPSHIFT_DOMAIN']}/chat/issues`, {
+    headers: {
+      Accept: "application/json",
+      Authorization: `Basic ${Buffer.from(env['HELPSHIFT_API_KEY']).toString('base64')}`
+    },
+    method: "POST",
+    // TODO: attachments?
+    body: getFormData({
+      'platform-id': env['HELPSHIFT_PLATFORM_ID'],
+      'identifier': From,
+      'end_user_id': From,
+      'message-body': Body
+    })
+  });
+  return await response.json();
+}
+
+// TODO: attachments
+async function sendMessageToAgent(issueId, { From, Body }) {
+  let response = await fetch(`https://api.helpshift.com/v1/${env['HELPSHIFT_DOMAIN']}/chat/issues/${issueId}/messages/user`, {
+    headers: {
+      Accept: "application/json",
+      Authorization: `Basic ${Buffer.from(env['HELPSHIFT_API_KEY']).toString('base64')}`
+    },
+    method: "POST",
+    body: getFormData({
+      'identifier': From,
+      'message-body': Body,
+      'message-type': "Text"
+    })
+  });
+  return await response.json();
+}
+
 function getFormData(object) {
   let formData = new FormData();
   for ( var key in object ) formData.append(key, object[key]);
   return formData;
+}
+
+async function setupMessageListener({ id, end_user_id }){
+  intervals[id] = setInterval(async () => {
+
+    let params = new URLSearchParams({ 
+      "identifier": end_user_id,
+      "messages-cursor": new Date().getTime() - CHECK_INTERVAL
+    });
+    let response = await fetch(`https://api.helpshift.com/v1/${env['HELPSHIFT_DOMAIN']}/chat/issues/${id}/messages?${params.toString()}`, {
+      headers: {
+        Accept: "application/json",
+        Authorization: `Basic ${Buffer.from(env['HELPSHIFT_API_KEY']).toString('base64')}`
+      }
+    });
+
+    let json = await response.json();
+    let { messages, issue_state_data } = json;
+    if (messages && issue_state_data) {
+      let resolved = issue_state_data['state'] === 'resolved';
+      let newMessages = messages.filter(message => message.origin === 'helpshift');
+
+      if (resolved) {
+        clearInterval(intervals[id]);
+        newMessages.push({ body: "This issue has been resolved" })
+      }
+
+      await sendNewMessagesToUser(end_user_id, newMessages);
+    } else {
+      // This only happens when we have hit the API Rate limit
+      // TODO: Retry
+      console.log('Rate limit hit', json);
+    }
+
+  }, CHECK_INTERVAL)
+}
+
+// TODO: attachements
+async function sendNewMessagesToUser(toPhoneNum, messages) {
+  for (let message of messages) {
+    try {
+      await twilioClient.messages.create({
+        to: toPhoneNum,
+        from: env['TWILIO_FROM_PHONE_NUMBER'],
+        body: message['body']
+      });
+    } catch (e) {
+      console.log("Error sending SMS", e)
+    }
+  }
 }
 
 app.listen(process.env.PORT || 5000, () => {
